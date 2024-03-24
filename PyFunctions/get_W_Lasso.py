23d787ad654ee64e68e1c885e1a9351335fe1d4e
fameen <fameen@ncsu.edu>
2024-03-24 01:12:05 -0400

    Great improvement to lasso function, used torch to speed up gradient descent

--- PyFunctions/get_W_Lasso.py
+++ PyFunctions/get_W_Lasso.py
@@ -8,25 +8,24 @@ def GetWLasso (X, Y, lam):
   '''
   model = Lasso_Regression(learning_rate = 0.02, 
                           no_of_iterations=10000,
-                         lambda_parameter=lam)
+                         lam=lam)
   
   W = torch.zeros((X.shape[1], Y.shape[1]), dtype=torch.float64)
   for k in range(0, Y.shape[1]):
     print(f"Processing weight {k}")
-    model.fit(X, Y[:, k].unsqueeze(1))
-    W[:, k] = model.getWeights()
+    W[:, k] = model.fit(X, Y[:, k].unsqueeze(1))
 
   return ( W, GetRSquared(X, Y, W) )
 
   
 class Lasso_Regression():
 
-  #initiating the hyperparameters
-  def __init__(self, learning_rate, no_of_iterations, lambda_parameter):
+  # initiating the hyperparameters
+  def __init__(self, learning_rate, no_of_iterations, lam):
     self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     self.learning_rate = learning_rate
     self.no_of_iterations = no_of_iterations
-    self.lambda_parameter = lambda_parameter
+    self.lam = lam
 
 
   # fitting the dataset to the Lasso Regression model
@@ -40,36 +39,34 @@ class Lasso_Regression():
     # implementing Gradient Descent algorithm for Optimization
     for i in range(self.no_of_iterations):
       self.update_weights()
+    
+    return self.w[:,0]
 
 
   # function for updating the weight & bias value
   def update_weights(self):
 
     # linear equation of the model
-    Y_prediction = self.predict(self.X)
+    Y_pred = self.X @ self.w + self.b
 
     # gradient for weight
     dw = torch.zeros((self.n, 1), dtype=torch.float64).to(self.device)
 
-    for i in range(self.n):
-      if self.w[i]>0:
-        dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_prediction)) + self.lambda_parameter) / self.m
-      else :
-        dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_prediction)) - self.lambda_parameter) / self.m
+    # for i in range(self.n):
+    #   if self.w[i]>0:
+    #     dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_pred)) + self.lam) / self.m
+    #   else :
+    #     dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_pred)) - self.lam) / self.m
+
+    dw = ( -(2*(self.X.T @ (self.Y - Y_pred))) - self.lam) / self.m
+    dw = torch.where( self.w > 0, dw + (2 * self.lam) / self.m, dw )
 
     # gradient for bias
-    db = - 2 * torch.sum(self.Y - Y_prediction) / self.m
+    db = - 2 * torch.sum(self.Y - Y_pred) / self.m
 
     # updating the weights & bias
     self.w = self.w - self.learning_rate*dw
     self.b = self.b - self.learning_rate*db
-
-  # Predicting the Target variable
-  def predict(self,X):
-    return X @ self.w + self.b
-  
-  def getWeights(self):
-    return self.w[:,0]
   
 
 def GetRSquared(X, Y, W):
