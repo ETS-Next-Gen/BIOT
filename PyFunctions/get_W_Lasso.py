b9aaf193f02bcb3a77c4647324b4ab7df5762b96
fameen <fameen@ncsu.edu>
2024-04-11 14:18:58 -0400

    Testing lasso on CUDA

--- PyFunctions/get_W_Lasso.py
+++ PyFunctions/get_W_Lasso.py
@@ -1,74 +1,124 @@
 import torch
 
-def GetWLasso (X, Y, lam, device="cpu"):
-  '''
-  X: predictor matrix
-  Y: response matrix
-  lambda: Lasso hyperparameter
-  '''
-  model = Lasso_Regression(learning_rate = 0.02, 
-                          no_of_iterations=10000,
-                         lam=lam,
-                         device=device)
-  
-  W = (torch.stack([model.fit(X, col) for col in Y.T]).to(device)).T
-  # W = torch.zeros((X.shape[1], Y.shape[1]), dtype=torch.float64)
-  # for k in range(0, Y.shape[1]):
-  #   print(f"Processing weight {k}")
-  #   W[:, k] = model.fit(X, Y[:, k].unsqueeze(1))
-
-  return ( W, GetRSquared(X, Y, W) )
-
-  
-class Lasso_Regression():
-
-  # initiating the hyperparameters
-  def __init__(self, learning_rate, no_of_iterations, lam, device):
-    self.device = device
-    self.learning_rate = learning_rate
-    self.no_of_iterations = no_of_iterations
-    self.lam = lam
-
-
-  # fitting the dataset to the Lasso Regression model
-  def fit(self, X, Y):
-    self.m, self.n = X.shape
-    self.w = torch.zeros((self.n, 1), dtype=torch.float64).to(self.device)
-    self.b = 0
-    self.X = X
-    self.Y = Y
-
-    # TODO: compare difference to epsilon to test for convergence ?
-    # implementing Gradient Descent algorithm for Optimization
-    for i in range(self.no_of_iterations):
-
-      # Make prediction
-      Y_pred = self.X @ self.w + self.b
-
-      # gradient for weight
-      # for i in range(self.n):
-      #   if self.w[i]>0:
-      #     dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_pred)) + self.lam) / self.m
-      #   else :
-      #     dw[i] = (-(2*(self.X[:,i].t()) @ (self.Y - Y_pred)) - self.lam) / self.m
-      dw = ( -(2*(self.X.T @ (self.Y - Y_pred))) - self.lam) / self.m
-      dw = torch.where( self.w > 0, dw + (2 * self.lam) / self.m, dw )
-
-      # gradient for bias
-      db = - 2 * torch.sum(self.Y - Y_pred) / self.m
-
-      # updating the weights & bias
-      self.w = self.w - self.learning_rate*dw
-      self.b = self.b - self.learning_rate*db
-    
-    return self.w[:,0]
-  
-
 def GetRSquared(X, Y, W):
   '''
   X: predictor matrix
   W: regression weights
   Y: response matrix
+
+  Returns the R^2. A value close to 0 indicates little correlation, while a value close to 1 indicates the opposite.
   '''
   
-  return 1 - (torch.sum((X @ W - Y)**2) / torch.sum((Y - torch.mean(Y))**2))
+  return 1 - (torch.sum((X @ W - Y)**2, dim=0) / torch.sum((Y - torch.mean(Y))**2, dim=0))
+
+
+def Lasso(X, Y, lambdas, maxIter=100000, device='cpu', alpha=0.1, intercept=False, thresh=1e-7):
+  '''
+  X: predictor matrix
+  Y: response matrix
+  lambdas: sequence of lambdas for regularization
+  maxIter: maximum number of iterations allowed
+  device: gpu or cpu?
+  alpha: learning rate
+  intercept: should the bias be calculated too?
+  thresh: convergence criteria
+
+  KEY POINT: Use lambdas of decreasing value. A higher first lambda returns higher r2, 
+    although Lasso runs more iterations and takes 10x more time.
+  Returns w, the regression weights, and the corresponding r^2 value.
+  '''
+
+  m, n = X.shape
+  l = Y.shape[1]
+  w = torch.zeros((n, l), dtype=torch.float64).to(device)
+  b = 0
+  prev_loss = 0
+  nLambdas = lambdas.size(0) - 1
+
+  # Implementing Gradient Descent algorithm for Optimization
+  for i in range(maxIter):
+    # print(f"Iter {i}")
+
+    # Make prediction
+    Y_pred = X @ w 
+
+    # Gradient for bias + updating bias
+    if intercept:
+      Y_pred += b
+      db = -(2 / m) * torch.sum(Y - Y_pred)
+      b = b - alpha * db
+
+    # Set lambda index
+    idx = nLambdas if i > nLambdas else i
+
+    # Update gradients for weight
+    dw = -(2 / m) * X.T @ (Y - Y_pred)
+    dw = torch.where( w > 0, dw - ((2 / m) * lambdas[idx]), dw + ((2 / m) * lambdas[idx]))
+
+    # Update the weights
+    w = w - alpha * dw
+
+    # Compute loss.... should it be torch.sum(.. , dim=0) ?
+    loss = (1 / (2 * m)) * torch.sum((Y - Y_pred) ** 2) + lambdas[idx] * torch.sum(torch.abs(w))
+
+    # Check convergence criterion
+    # print(f"Iter : {i}, diff : {prev_loss - loss}, lambda: {lambdas[idx]}")
+    if abs(prev_loss - loss) < thresh:  # You can adjust this threshold as needed
+      return w, GetRSquared(X, Y, w)
+
+    # Update loss
+    prev_loss = loss
+  
+  print("WARNING: Convergence criteria not met. Returning regression weights anyway.")
+  return w, GetRSquared(X, Y, w)
+
+
+
+# TESTS + PROFILING
+def runtime():
+  import numpy as np
+  import time
+  from sklearn.preprocessing import StandardScaler
+  import matplotlib.pyplot as plt
+  from guppy import hpy; h=hpy()
+
+  DatasetPath = "Datasets/dataset.csv"
+  EmbeddingPath = "Datasets/embedding.csv"
+
+  # heap_status1 = h.heap()
+
+  X1 = StandardScaler(with_std=False).fit_transform(np.genfromtxt(EmbeddingPath, delimiter=',', dtype='float64'))
+  Fe1 = StandardScaler().fit_transform(np.genfromtxt(DatasetPath, delimiter=',', skip_header=1, dtype='float64'))
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  print(f"Device: {device}")
+  X = torch.tensor(X1, device=device)
+  Fe = torch.tensor(Fe1, device=device)
+  # lambdas = torch.tensor([10000, 1000, 500, 300, 200, 100, 50, 10, 1, 0.0001], dtype=torch.float64)
+  lambdas = torch.tensor([100, 0.0001], dtype=torch.float64, device=device)
+
+  # W = Lasso(Fe, X, lambdas)
+
+  # heap_status2 = h.heap()
+  # print(heap_status2.size - heap_status1.size)
+
+  K = 5
+  tot = 0
+  times = []
+  for i in range(K):
+
+    s = time.time()
+
+    W, r2 = Lasso(Fe, X, lambdas, device=device)
+
+    elapsed = time.time() - s
+    times.append(elapsed)
+    print(elapsed)
+    tot += elapsed
+
+
+  r2 = GetRSquared(Fe, X, W)
+  avg = torch.sum(r2) / (384)
+  print(f"Average r2: {avg}")
+  
+runtime()
+
